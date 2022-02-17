e2c7d166583444d1e677f359efe196181bf9aa0f
Jagolen <jakob.golen@gmail.com>
2022-02-17 19:37:38 +0100

    Task1 and 2 done in lab 8

--- Labs/Lab8/Task-1/threadtest1.c
+++ Labs/Lab8/Task-1/threadtest1.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void* the_thread_func(void* arg) {
-  /* Do something here? */
+void* the_thread_func_B(void* arg) {
+  printf("The thread function B is starting doing some work\n");
+  long int k;
+  double sum3;
+  for(k = 0; k < 10000000000; k++) sum3 += 3e-7;
+  printf("Result of work done in the thread function B: sum3 = %lf\n",sum3);
+  return NULL;
+}
+
+void* the_thread_func_A(void* arg) {
+  printf("The thread function A is starting doing some work\n");
+  long int j;
+  double sum2;
+  for(j = 0; j < 10000000000; j++) sum2 += 2e-7;
+  printf("Result of work done in the thread function A: sum2 = %lf\n",sum2);
   return NULL;
 }
 
@@ -11,16 +24,23 @@ int main() {
 
   /* Start thread. */
   pthread_t thread;
+  pthread_t thread2;
   printf("the main() function now calling pthread_create().\n");
-  pthread_create(&thread, NULL, the_thread_func, NULL);
+  pthread_create(&thread, NULL, the_thread_func_A, NULL);
+  pthread_create(&thread2, NULL, the_thread_func_B, NULL);
 
   printf("This is the main() function after pthread_create()\n");
 
-  /* Do something here? */
+  printf("Main starting to do some work!\n");
+  long int i;
+  double sum;
+  for(i = 0; i < 10000000000; i++) sum += 1e-7;
+  printf("Result of work done in main(): sum = %lf\n",sum);
 
   /* Wait for thread to finish. */
   printf("the main() function now calling pthread_join().\n");
   pthread_join(thread, NULL);
+  pthread_join(thread2, NULL);
 
   return 0;
 }
--- Labs/Lab8/Task-2/thread_data.c
+++ Labs/Lab8/Task-2/thread_data.c
@@ -2,7 +2,8 @@
 #include <pthread.h>
 
 void* the_thread_func(void* arg) {
-  /* Do something here? */
+  double* ptr = (double*) arg;
+  printf("Data for thread contains: [%lf, %lf, %lf]\n",ptr[0],ptr[1],ptr[2]);
   return NULL;
 }
 
@@ -14,10 +15,17 @@ int main() {
   data_for_thread[1] = 9.2;
   data_for_thread[2] = 1.6;
 
+  double data_for_thread2[3];
+  data_for_thread2[0] = 56.7;
+  data_for_thread2[1] = 90.2;
+  data_for_thread2[2] = 12.6;
+ 
+
   /* Start thread. */
   pthread_t thread;
   printf("the main() function now calling pthread_create().\n");
   pthread_create(&thread, NULL, the_thread_func, data_for_thread);
+  pthread_create(&thread,NULL,the_thread_func,data_for_thread2);
 
   printf("This is the main() function after pthread_create()\n");
 
