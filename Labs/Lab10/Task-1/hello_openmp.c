4b4b47317a216f9df70e254692c9cc84cde4666d
Jagolen <jakob.golen@gmail.com>
2022-02-24 13:59:38 +0100

    Task 1 Lab 10 done

--- Labs/Lab10/Task-1/hello_openmp.c
+++ Labs/Lab10/Task-1/hello_openmp.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <omp.h>
 
 int main(int argc, char** argv) {
+omp_set_num_threads(7);
 
-#pragma omp parallel num_threads(5)
+#pragma omp parallel
   {
     printf("Bonjour!\n");
   }
